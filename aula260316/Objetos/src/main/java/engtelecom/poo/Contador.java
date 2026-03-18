2c0b1292cbb5bee8d8861a4f6038292fe4b0afa2
Mi.Claudino <mifloripa456@gmail.com>
2026-03-18 07:18:16 -0300

    Adicionando contador e suas funcionalidades.

---
+++ aula260316/Objetos/src/main/java/engtelecom/poo/Contador.java
@@ -0,0 +1,19 @@
+package engtelecom.poo;
+
+public class Contador{
+
+    private int contagem;
+
+    public void aumentar(){
+        contagem++;
+    }
+
+    public int getContagem(){
+        return contagem;
+    }
+
+    public void setContagem(int n){
+        contagem = n;
+    }
+}
+
