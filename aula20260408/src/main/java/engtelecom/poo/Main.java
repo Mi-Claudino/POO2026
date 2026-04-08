87c6243b11a33430a36b7de7ee8c317695e89039
mi-claudino <micehl.p2004@aluno.ifsc.edu.br>
2026-04-08 11:27:30 -0300

    Treino de HashMap, arraylist e streams.

---
+++ aula20260408/src/main/java/engtelecom/poo/Main.java
@@ -0,0 +1,41 @@
+package engtelecom.poo;
+
+
+import java.awt.image.ImagingOpException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Main {
+    static void main() {
+
+//
+//        HashMap<Integer, String> mapa = new HashMap<>();
+//
+////        // String s = mapa.put(123, "Java");
+////
+////        String s = mapa.put(123,"Java");
+////        if (s==null){
+////            IO.println("primeira vez que a chave 123 foi usada");
+////        }
+////
+////        s = mapa.put(123, "C++");
+////
+////        IO.println("fim");
+//
+//        mapa.put(123, new String("Java"));
+//        mapa.put(456, "C++");
+//        mapa.put(789, "Java");
+//
+//
+//
+//
+
+
+        List<String> lista = Array.asList("Java", "Stream", )
+
+
+
+
+
+    }
+}
